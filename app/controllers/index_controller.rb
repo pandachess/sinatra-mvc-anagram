6f3c5eec8331f50d5f052ddc7bace0b2e365de9b
Jacqueline Chapman <jacquelinechapman@me.com>
2015-03-31 11:07:55 -0400

    Checking the database and only returning words to user

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -6,7 +6,7 @@ end
 
 get '/anagrams/:word' do
 	@word = params[:word]
-	@anagrams = Word.find_anagram(@word)
+	@anagrams = Word.find_anagrams(@word)
 	erb :show
 end
 
--- app/models/word.rb
+++ app/models/word.rb
@@ -1,9 +1,9 @@
 class Word < ActiveRecord::Base
 
-	def self.find_anagram(word)
+	def self.find_anagrams(word)
 	  word_array = word.chars.map(&:to_s)
 	  reordered_words = word_array.permutation.map(&:join)
-	  return reordered_words
+	  Word.where("text in (?)", reordered_words)
 	end
 
 end
